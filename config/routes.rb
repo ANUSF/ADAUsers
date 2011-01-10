f352ec7b108ac24bdb885f79b685ae94f49a98f0
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-01-10 17:51:44 +1100

    No catch-all routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 OpenidServer::Application.routes.draw do
   match 'server/xrds',         :to => 'identities#idp_xrds'
+  match 'server/index',        :to => 'identities#index'
+  match 'server/decision',     :to => 'identities#decision'
   match 'user/*username',      :to => 'identities#user_page', :format => false
   match 'user/*username/xrds', :to => 'identities#user_xrds', :format => false
-  match 'server/:action',      :to => 'identities'
 end
