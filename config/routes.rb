271b3df4d6b8659cd0b15349410994d048508623
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-01-14 18:05:20 +1100

    Special XRDS routes call normal controller actions via rack magic.

--- config/routes.rb
+++ config/routes.rb
@@ -10,39 +10,16 @@ OpenidServer::Application.routes.draw do
   match 'user/*username', :to => 'users#show', :as => 'user', :format => false
 
 
-  # Handle some XRDS requests via Rack so that the content type is set correctly
+  # A bit of trickery to serve XRDS from bare URLs
 
-  def xrds_text(env, mode)
-    types = if mode == 'idp'
-              [ OpenID::OPENID_IDP_2_0_TYPE ]
-            else
-              [ OpenID::OPENID_2_0_TYPE,
-                OpenID::OPENID_1_0_TYPE,
-                OpenID::SREG_URI         ]
-            end
-    type_str = types.map { |uri| "<Type>#{uri}</Type>" }.join "\n      "
-    server = env['REQUEST_URI'].sub(/#{env['PATH_INFO']}$/, '/server')
-
-    %Q!<?xml version="1.0" encoding="UTF-8"?>
-<xrds:XRDS
-    xmlns:xrds="xri://$xrds"
-    xmlns="xri://$xrd*($v*2.0)">
-  <XRD>
-    <Service priority="0">
-      #{type_str}
-      <URI>#{server}</URI>
-    </Service>
-  </XRD>
-</xrds:XRDS>
-!
-  end
-
-  match 'xrds/user', :to => lambda { |env|
-    [200, { "Content-Type" => "application/xrds+xml" }, [xrds_text env, 'user']]
+  match 'xrds/user/*username', :to => lambda { |env|
+    env['HTTP_ACCEPT'] = 'application/xrds+xml'
+    UsersController.action(:show).call(env)
   }
 
   match 'xrds/idp', :to => lambda { |env|
-    [ 200, { "Content-Type" => "application/xrds+xml" }, [xrds_text env, 'idp']]
+    env['HTTP_ACCEPT'] = 'application/xrds+xml'
+    IdentitiesController.action(:root).call(env)
   }
 
   root :to => 'identities#root'
