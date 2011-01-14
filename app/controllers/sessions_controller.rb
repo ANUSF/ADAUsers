4d1629ace451a5d581390aad675f504c5fdd92c5
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-01-14 17:22:53 +1100

    Tiny change.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,7 @@ class SessionsController < ApplicationController
   def new
     @oidreq = session[:last_oidreq]
     if @oidreq.id_select
-      Rails.logger.error "@@@ got an id_select request"
-    elsif username_for(@oidreq.identity).blank?
-      Rails.logger.error "@@@ no username given"
+      nil
     end
   end
 
