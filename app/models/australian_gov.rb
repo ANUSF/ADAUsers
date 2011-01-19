fa6da0796d6f006ec5cf5247db17efd94208d7a1
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-01-19 15:31:18 +1100

    Continued work on user registration page.

---
+++ app/models/australian_gov.rb
@@ -0,0 +1,4 @@
+class AustralianGov < ActiveRecord::Base
+  set_table_name 'agenciesdept'
+  set_inheritance_column false
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ActiveRecord::Base
 
   belongs_to :country, :foreign_key => 'countryid'
   belongs_to :australian_uni, :foreign_key => 'uniid'
+  belongs_to :australian_gov, :foreign_key => 'departmentid'
 
   # def after_find; readonly! end
 
-  attr_accessor :email_verify
+  attr_accessor :email_verify, :other_australian_affiliation,
+                :other_australian_type
 
   def title_options
     ['Mr', 'Mrs', 'Miss', 'Ms', 'Dr' ]
@@ -40,5 +42,17 @@ class User < ActiveRecord::Base
 
   def austinstitution_options
     [ ['University', 'Uni'], ['Government/Research', 'Dept'], 'Other' ]
+  end
+
+  def other_aust_inst_types
+    [ "Educational institution",
+      "Federal government department",
+      "Federal government agency",
+      "State Government",
+      "Media",
+      "Not for profit organization",
+      "Private company",
+      "None - personal user"
+    ]
   end
 end
