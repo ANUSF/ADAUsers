0803bc6fe6da51a4b2709b676270ce1cb825b284
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-01-19 12:52:37 +1100

    Continued work on user registration page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,42 @@ class User < ActiveRecord::Base
   set_table_name 'userdetails'
   set_primary_key 'user'
 
-  attr_accessor :email_verify
+  belongs_to :country, :foreign_key => 'countryid'
 
   # def after_find; readonly! end
+
+  attr_accessor :email_verify
+
+  def title_options
+    ['Mr', 'Mrs', 'Miss', 'Ms', 'Dr' ]
+  end
+
+  def position_options
+    [ "Undergraduate student",
+      [ "PhD/Postgraduate student", "Postgraduate student" ],
+      [ "Other student (e.g. secondary school/high school)", "Other student" ],
+      [ "Lecturer/teacher", "Lecturer" ],
+      "Researcher",
+      "Research Assistant",
+      [ "Personal user (no institutional, " +
+        "company or organizational affiliation)", "Personal" ],
+      "Other"
+    ]
+  end
+
+  def action_options
+    [ [ "Thesis or coursework", "coursework" ],
+      "Teaching",
+      "Pure research",
+      "Research Consultancy",
+      [ "Government research", "Government" ],
+      [ "Commercial research", "Commercial" ],
+      [ "Personal interest", "Personal" ],
+      "Other"
+    ]
+  end
+
+  def austinstitution_options
+    [ ['University', 'Uni'], ['Government/Research', 'Dept'], 'Other' ]
+  end
 end
