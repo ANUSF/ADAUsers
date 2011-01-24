42c0e4d35785c7b4770f37ce288c0721272bef83
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-01-24 15:39:48 +1100

    More work on conditional registration form field display.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,37 +1,61 @@
-/* DO NOT MODIFY. This file was compiled Mon, 24 Jan 2011 02:35:40 GMT from
+/* DO NOT MODIFY. This file was compiled Mon, 24 Jan 2011 04:39:11 GMT from
  * /home/olaf/Ruby-Rails/openid-server/app/coffeescripts/application.coffee
  */
 
 (function() {
-  var $, country_change;
-  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  var $, checked_radio_button, country_change, institution_change, patterns;
   $ = jQuery;
-  country_change = function(item) {
-    var form;
-    form = item.closest('form#new_user');
-    if (item.val() === "15") {
-      form.find('#australia-inputs').css({
-        display: 'block'
-      });
-      return form.find('#not-australia-inputs').css({
-        display: 'none'
-      });
-    } else {
-      form.find('#australia-inputs').css({
-        display: 'none'
-      });
-      return form.find('#not-australia-inputs').css({
-        display: 'block'
-      });
+  patterns = {
+    form: 'form#new_user',
+    country_selection: '#user_country_input select',
+    affiliation_aus: '#affiliation-aus',
+    affiliation_non_aus: '#affiliation-non-aus',
+    inst_type_container: '#user_austinstitution_input',
+    inst_buttons_name: 'user[austinstitution]',
+    inst_type_conditional_fields: {
+      Uni: '#user_australian_uni_input',
+      Dept: '#user_australian_gov_input',
+      Other: '#user_australian_other_inputs'
     }
   };
+  checked_radio_button = function(scope, name) {
+    return scope.find("input[@name='" + name + "']:checked");
+  };
+  country_change = function(item) {
+    var aus, form, other;
+    form = item.closest(patterns.form);
+    aus = form.find(patterns.affiliation_aus);
+    other = form.find(patterns.affiliation_non_aus);
+    if (item.val() === "15") {
+      aus.show();
+      return other.hide();
+    } else {
+      aus.hide();
+      return other.show();
+    }
+  };
+  institution_change = function(item) {
+    var form, k, v, value, _ref, _results;
+    form = item.closest(patterns.form);
+    value = checked_radio_button(form, patterns.inst_buttons_name).val();
+    _ref = patterns.inst_type_conditional_fields;
+    _results = [];
+    for (k in _ref) {
+      v = _ref[k];
+      _results.push(form.find(v)[k === value ? 'show' : 'hide']());
+    }
+    return _results;
+  };
   $(document).ready(function() {
-    return $('form#new_user #user_country_input select').each(function() {
+    $(patterns.form).find(patterns.country_selection).each(function() {
       return country_change($(this));
     }).change(function() {
-      return setTimeout((__bind(function() {
-        return country_change($(this));
-      }, this)), 200);
+      return country_change($(this));
+    });
+    return $(patterns.form).find(patterns.inst_type_container).each(function() {
+      return institution_change($(this));
+    }).find('input').click(function() {
+      return institution_change($(this));
     });
   });
 }).call(this);
