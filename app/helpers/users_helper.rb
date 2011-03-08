ed0d91aac490d59e548484f5630bb968a60b692f
Rohan Mitchell <rohan.mitchell@anu.edu.au>
2011-03-08 14:33:52 +1100

    Split out Australian gov options generation into a helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,8 @@ module UsersHelper
       yield
     end
   end
+
+  def build_australian_gov_options
+    AustralianGov.types.map {|t| {t.name => t.members.map {|m| [m.name, m.id]}}}.reduce(:merge).reject {|k,v| k == ''}
+  end
 end
