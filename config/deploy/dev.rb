9fd0da751b3218a91ebd5ac45cb567523929cc73
Rohan Mitchell <rohan.mitchell@anu.edu.au>
2011-04-29 14:04:46 +1000

    Added testada-specific deployment info file

---
+++ config/deploy/dev.rb
@@ -0,0 +1,13 @@
+set :application, "staging"
+
+role :web, "testada"                          # Your HTTP server, Apache/etc
+role :app, "testada"                          # This may be the same as your `Web` server
+role :db,  "testada", :primary => true # This is where Rails migrations will run
+
+set :rails_env, "development"
+
+set :deploy_to,   "/data"
+
+set :user,        "deploy"
+set :use_sudo,    true
+set :deploy_to,   "/data"
