fd48a3ab9c0bec1f2548a1d699df8fa6b688b521
Rohan Mitchell <rohan.mitchell@anu.edu.au>
2011-05-31 10:35:50 +1000

    Minor formatting fixes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -24,7 +24,7 @@ class UserMailer < ActionMailer::Base
   def pending_datasets_access_approved_email(user, category)
     @user = user
     @datasets = category == :a ? @user.datasets_cat_a_pending_to_grant : @user.datasets_cat_b_pending_to_grant
-    @datasets.map! {|datasetID| AccessLevel.find_by_datasetID(datasetID)}
+    @datasets.map! { |datasetID| AccessLevel.find_by_datasetID(datasetID) }
 
     mail(:to => user.email, :subject => "Access approved for #{category == :a ? "General" : "Restricted"} dataset(s)")
   end
