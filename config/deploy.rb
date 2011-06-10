d847c14fe4e95bb879ae4c2701352e099e2172a8
Rohan Mitchell <rohan.mitchell@anu.edu.au>
2011-06-10 15:27:41 +1000

    Changed capistrano copy_database_yml task to copy_secrets, and symlink secrets.rb on deploy. For deployment, simply: cap copy_secrets; cap deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,11 +47,11 @@ task :create_extra_dirs, :roles => :app do
   run "mkdir -m 0755 -p #{shared_path}/db"
 end
 
-desc "copy the database configuration to the server"
-task :copy_database_yml, :roles => :app do
-  prompt = "Specify a database configuration file to copy to the server:"
+desc "copy the secret configuration variables to the server"
+task :copy_secrets, :roles => :app do
+  prompt = "Specify a secrets.rb file to copy to the server:"
   path = Capistrano::CLI.ui.ask prompt
-  put File.read("#{path}"), "#{shared_path}/database.yml", :mode => 0600
+  put File.read("#{path}"), "#{shared_path}/secrets.rb", :mode => 0600
 end
 
 task :echo_ruby_env do
@@ -62,7 +62,7 @@ end
 
 task :symlinks, :roles => :app do
   run "ln -nfs #{shared_path}/db/* #{current_path}/db/"
-  run "ln -nfs #{shared_path}/database.yml #{current_path}/config/"
+  run "ln -nfs #{shared_path}/secrets.rb #{current_path}/config/initializers/"
 end
 
 task :deploy_log, :roles => :app do
