509b6d2ecb91174512fa53dacf41e5524d0182df
Rohan Mitchell <rohan.mitchell@anu.edu.au>
2011-06-15 10:50:18 +1000

    First cut of migration to add CMS role to userdetails

--- db/migrate/20110504005514_add_role_cms_to_userdetails.rb
+++ db/migrate/20110504005514_add_role_cms_to_userdetails.rb
@@ -2,7 +2,26 @@ class AddRoleCmsToUserdetails < ActiveRecord::Migration
   def self.up
     add_column :userdetails, :role_cms, :string
 
-    # TODO: Map existing users' nesstar roles to a CMS role
+    # Map existing users' nesstar roles to a CMS role
+    UserWithoutValidations.find_in_batches(:batch_size => 250) do |users|
+      say "Setting CMS role for users #{users.first.user} to #{users.last.user}..."
+      users.each do |user|
+        user.role_cms = case user.user_role
+                        when 'administrator'
+                          'administrator'
+                        when 'publisher'
+                          # TODO: Multiple values?
+                          ['manager', 'approver']
+                        when /fullauthorisedUser|authorisedUser|affiliateusers/
+                          'member'
+                        else
+                          say "Unknown role #{user.user_role} for user #{user.user}."
+                          nil
+                        end
+        user.save!
+      end
+    end
+    say "Done!"
   end
 
   def self.down
