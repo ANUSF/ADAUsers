6eb4d7b1c039ced2ddd981fafe970c106a3e8374
Rohan Mitchell <rohan.mitchell@anu.edu.au>
2011-06-21 11:21:33 +1000

    Initialise role_cms for all users through migration

--- db/migrate/20110504005514_add_role_cms_to_userdetails.rb
+++ db/migrate/20110504005514_add_role_cms_to_userdetails.rb
@@ -3,22 +3,25 @@ class AddRoleCmsToUserdetails < ActiveRecord::Migration
     add_column :userdetails, :role_cms, :string
 
     # Map existing users' nesstar roles to a CMS role
-    UserWithoutValidations.find_in_batches(:batch_size => 250) do |users|
+    UserWithoutValidations.find_in_batches(:batch_size => 300) do |users|
       say "Setting CMS role for users #{users.first.user} to #{users.last.user}..."
       users.each do |user|
-        user.role_cms = case user.user_role
-                        when 'administrator'
-                          'administrator'
-                        when 'publisher'
-                          # TODO: Multiple values?
-                          ['manager', 'approver']
-                        when /fullauthorisedUser|authorisedUser|affiliateusers/
-                          'member'
-                        else
-                          say "Unknown role #{user.user_role} for user #{user.user}."
-                          nil
-                        end
-        user.save!
+        begin
+          user.role_cms = case user.user_role
+                          when 'administrator'
+                            'administrator'
+                          when 'publisher'
+                            'approver'
+                          when /fullauthorisedUser|authorisedUser|affiliateusers/
+                            'member'
+                          else
+                            say "Unknown role #{user.user_role} for user #{user.user}."
+                            nil
+                          end
+          user.save!
+        rescue
+          say "Skipping #{user.user} - error saving their record (check for non-UTF8 chars in their username)"
+        end
       end
     end
     say "Done!"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110504005514) do
+ActiveRecord::Schema.define(:version => 20110606000548) do
 
   create_table "ProjectEJB", :id => false, :force => true do |t|
     t.text     "comment"
