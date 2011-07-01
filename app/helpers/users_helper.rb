c2a9d8cf428b41823170c92c4b2046e4f4963613
Rohan Mitchell <rohan.mitchell@anu.edu.au>
2011-07-01 10:27:57 +1000

    Position Australia at the top of the list on the registration form

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,9 @@ module UsersHelper
   def build_australian_gov_options
     AustralianGov.types.map {|t| {t.name => t.members.map {|m| [m.name, m.id]}}}.reduce(:merge).reject {|k,v| k == ''}
   end
+
+  # Return countries with Australia positioned first
+  def countries_options
+    Country.all.reject { |c| c == User::AUSTRALIA }.unshift(User::AUSTRALIA, Country.new)
+  end
 end
