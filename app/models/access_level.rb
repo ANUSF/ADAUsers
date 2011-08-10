03c6ecec5cc246bdb217ae0b5aed7c8c02403121
Rohan Mitchell <rohan.mitchell@anu.edu.au>
2011-08-11 09:58:26 +1000

    Added grouping to access level queries to remove duplicates

--- app/models/access_level.rb
+++ app/models/access_level.rb
@@ -8,9 +8,10 @@ class AccessLevel < ActiveRecord::Base
 
   scope :cat_a, where(:accessLevel => ['A', 'G'], :fileID => nil)
     .where("datasetID NOT IN (SELECT datasetID FROM accesslevel al2 WHERE al2.accesslevel in ('B', 'S') and al2.fileID is NULL)")
+    .group(:datasetID)
     .order('datasetID ASC')
 
-  scope :cat_b, where(:accessLevel => ['B', 'S'], :fileID => nil).order('datasetID ASC')
+  scope :cat_b, where(:accessLevel => ['B', 'S'], :fileID => nil).group(:datasetID).order('datasetID ASC')
 
   scope :files_for_dataset, (lambda do |datasetID, category|
     where("datasetID = ? AND accessLevel IN (?) AND fileID IS NOT NULL", datasetID, CATEGORY_ACCESS_LEVELS[category]).order('fileID')
