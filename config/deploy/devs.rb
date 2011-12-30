e09c42a84652fbd50f9ac2719aed90001bc13155
odf <olaf.delgado@gmail.com>
2011-12-30 18:36:54 +1100

    Updated the deploy config scripts.

--- config/deploy/devs.rb
+++ config/deploy/devs.rb
@@ -3,9 +3,9 @@ require 'rvm/capistrano'
 
 set :rvm_ruby_string, 'ruby-1.9.2-p180'
 
-role :web, "web2-nci.anu.edu.au"
-role :app, "web2-nci.anu.edu.au"
-role :db,  "web2-nci.anu.edu.au", :primary => true
+role :web, "web2.mgmt"
+role :app, "web2.mgmt"
+role :db,  "web2.mgmt", :primary => true
 
 set :user,        "d10web"
 set :use_sudo,    false
