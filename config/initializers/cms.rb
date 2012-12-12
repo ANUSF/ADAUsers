ac6def23cde91be30e6b998a856f9566c26428cb
odf <olaf.delgado@gmail.com>
2012-12-12 17:02:22 +1100

    CMS::HOST variable points to ada.edu.au when RAILS_ENV=staff

--- config/initializers/cms.rb
+++ config/initializers/cms.rb
@@ -1,3 +1,3 @@
 module CMS
-  HOST = {'development' => 'localhost:3000', 'test' => 'localhost:3000', 'devs' => 'test.ada.edu.au', 'staff' => 'staff.ada.edu.au'}
+  HOST = {'development' => 'localhost:3000', 'test' => 'localhost:3000', 'devs' => 'test.ada.edu.au', 'staff' => 'ada.edu.au'}
 end
